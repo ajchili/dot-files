44f120bec72f2db72cad0605808c64a4c4a1f8e4
Kirin Patel <kirinpatel@gmail.com>
2025-06-15 11:34:30 -0400

    🔧 update setup.sh script to include VSCode and zsh changes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-ln -sf .config/git ~/.git
-ln -sf .vimrc ~/.vimrc
+ln -sf "$PWD/.config/git" ~/.git
+
+dot_files=(".vimrc" ".zshrc")
+for file in "${dot_files[@]}"; do
+    ln -sf "$PWD/$file" ~/$file
+done
+
+# VSCode
+extensions=("esbenp.prettier-vscode" \
+    "streetsidesoftware.code-spell-checker" \
+    "usernamehw.errorlens" \
+    "eamodio.gitlens"\
+    "dbaeumer.vscode-eslint" \
+    "ms-azuretools.vscode-docker" \
+    "johnpapa.vscode-peacock" \
+    "ms-vscode.remote-explorer" \
+    "ms-vscode-remote.remote-ssh" \
+    "ms-vscode-remote.remote-ssh-edit" \
+    "pnp.polacode" \
+    "oderwat.indent-rainbow" \
+    "gruntfuggly.todo-tree" \
+    "mhutchie.git-graph" \
+    "vscode-icons-team.vscode-icons")
+
+for extension in "${extensions[@]}"; do
+    code --install-extension "$extension"
+done
+
+themes=("wesbos.theme-cobalt2" "zhuangtongfa.material-theme")
+
+for theme in "${themes[@]}"; do
+    code --install-extension "$theme"
+done
+
+cp -r .config/Code ~/Library/Application\ Support
