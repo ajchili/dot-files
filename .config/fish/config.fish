36a7b86f76b1b76a97949057b964edd0572d1192
Kirin Patel <kirin.patel@datavant.com>
2025-11-24 11:29:20 -0500

    make fish platform agnostic

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+set -l os (uname)
+switch (uname)
+    case Darwin
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    case Linux
+        eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+end
+
+set PATH $PATH "$HOME/.local/bin"
