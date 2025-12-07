3391b104dc6111c6933cdb0f8212d242b4576e5f
Kirin Patel <kirinpatel@gmail.com>
2025-12-07 11:30:28 -0500

    fix: refresh fish after install omf, allow for omf installs in same script

--- .config/fish/setup.sh
+++ .config/fish/setup.sh
@@ -1,5 +1,8 @@
 # https://github.com/oh-my-fish/oh-my-fish
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+refresh
+
 # https://github.com/edc/bass
 omf install bass
 omf install nvm
