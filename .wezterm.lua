31faf72bf0413a9915af0d902da442073797dea0
Kirin Patel <kirin.patel@datavant.com>
2026-01-05 10:04:56 -0500

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,9 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 28
 
-config.font_size = 16
+config.font_size = 18
 config.font = wezterm.font 'ComicShannsMono Nerd Font Mono'
+
+config.color_scheme = 'Cobalt2'
 
 return config
